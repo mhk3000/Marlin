a2adacb5bd41aadee85016632f7d1712af032775
sagardivate <sagard@mahersoft.com>
2013-06-13 10:21:58 +0530

    1. Function definitions for sdfile and error functions 2. Change in return to status counter timing for proper diplay of screen

--- Marlin/ultralcd.h
+++ Marlin/ultralcd.h
@@ -10,18 +10,24 @@
   void lcd_setstatus(const char* message);
   void lcd_setstatuspgm(const char* message);
   void lcd_setalertstatuspgm(const char* message);
+  void LCD_FILENAME(const char* name);//function added for showing the filename of the sd_card selected for printing
   void lcd_reset_alert_level();
+  //void lcd_error(const char* message);
   
   static unsigned char blink = 0;	// Variable for visualisation of fan rotation in GLCD
 
   #define LCD_MESSAGEPGM(x) lcd_setstatuspgm(PSTR(x))
   #define LCD_ALERTMESSAGEPGM(x) lcd_setalertstatuspgm(PSTR(x))
+  //#define LCD_ERROR(x) lcd_error(PSTR(x))
 
   #define LCD_UPDATE_INTERVAL 100
-  #define LCD_TIMEOUT_TO_STATUS 15000
+  
+  #define LCD_TIMEOUT_TO_STATUS 300000
+  
 
   #ifdef ULTIPANEL
   void lcd_buttons_update();
+  extern volatile uint8_t buttons;  //the last checked buttons in a bit array.
   #else
   FORCE_INLINE void lcd_buttons_update() {}
   #endif
@@ -34,8 +40,25 @@
   extern int absPreheatHPBTemp;
   extern int absPreheatFanSpeed;
     
-  void lcd_buzz(long duration,uint16_t freq);
-  bool lcd_clicked();
+  #ifdef NEWPANEL
+    #define EN_C (1<<BLEN_C)
+    #define EN_B (1<<BLEN_B)
+    #define EN_A (1<<BLEN_A)
+
+    #define LCD_CLICKED (buttons&EN_C)
+  #else
+    //atomatic, do not change
+    #define B_LE (1<<BL_LE)
+    #define B_UP (1<<BL_UP)
+    #define B_MI (1<<BL_MI)
+    #define B_DW (1<<BL_DW)
+    #define B_RI (1<<BL_RI)
+    #define B_ST (1<<BL_ST)
+    #define EN_B (1<<BLEN_B)
+    #define EN_A (1<<BLEN_A)
+    
+    #define LCD_CLICKED ((buttons&B_MI)||(buttons&B_ST))
+  #endif//NEWPANEL
 
 #else //no lcd
   FORCE_INLINE void lcd_update() {}
@@ -43,10 +66,10 @@
   FORCE_INLINE void lcd_setstatus(const char* message) {}
   FORCE_INLINE void lcd_buttons_update() {}
   FORCE_INLINE void lcd_reset_alert_level() {}
-  FORCE_INLINE void lcd_buzz(long duration,uint16_t freq) {}
 
   #define LCD_MESSAGEPGM(x) 
-  #define LCD_ALERTMESSAGEPGM(x) 
+  #define LCD_ALERTMESSAGEPGM(x)
+  //#define LCD_ERROR(x) 
 #endif 
 
 char *itostr2(const uint8_t &x);
